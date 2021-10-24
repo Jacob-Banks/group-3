e69d1883e891d3333b1b0203410eb758e3365446
Rayan Diouf <rayandiouf@Rayans-MacBook-Pro.local>
2021-10-24 11:43:55 -0400

    working on the auto-fill feature in Grommers component

--- client/src/components/Groomers/index.js
+++ client/src/components/Groomers/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 const MeetTheGroomers = ({ options }) => {
     const [Groomers] =useState([
         {
@@ -11,6 +10,16 @@ const MeetTheGroomers = ({ options }) => {
             photo: '../../images/b602a8_711782e419e944df8e0b286ce6da1ba1_mv2.webp',
             name: 'Elizabeth Santon',
             bio: "Elizabeth has loved animals all of her life, always gravitating towards dogs even at a young age.  Growing up with cats and dogs, she eventually pursued dog training before transitioning into dog grooming.  Elizabeth draws upon her training background and understanding of dog behavior to assist her during the grooming process.  Elizabeth's professional background coupled with her naturally calm demeanor, gives her the ability to help keep dogs calm; specifically dogs who are aggressive and anxious.  Elizabeth will also be leading our dog training program and uses a balanced approach in achieving goals for pets, as well as working dogs.  Along with offering basic obedience training, she will be offering Intermediate and Advanced Therapy Dog preparation, and assist with service dog training.  Elizabeth lives in East Garafraxa with her spouse, service dog Layne, and three cats: Prudence, Maggie and Floyd... and fish."
+        },
+        {
+            photo: '../../images/groomer-clipping-dogs-nails_jpeg-600x390.jpeg',
+            name: 'Viviane Curry',
+            bio: "Viviane has always had a love for animals. After a 15 year career in the IT industry, she decided to follow her passion and opened a successful dog walking and pet sitting company. Through the services she provided, Viviane gained invaluable experience handling and caring for a variety of dogs with different personalities and behaviors. Viviane now brings that handling experience to her grooming. Viviane shows each dog patience and understanding during the grooming process and treats each one like it was her own. Viviane lives in Schomberg with her daughter and two dogs Milo and India."
+        },
+        {
+            photo:'../../images/Dog-Grooming.jpeg',
+            name: 'Marie France',
+            bio: "Marie has had a lifelong love for animals, and now after a full career in the Tech Industry, she can finally fulfill her passion to work with clients of the 'fluffy' variety.  Marie is passionate about big and small animals (she cannot pass one by without giving them love and affection!) It is this passion that makes Marie an asset to the team. Marie is also a devoted pet owner, which helps her connect with your pets needs.  Marie currently resides in Fergus with her Family and her cat Shadow. "
         }
         
     ]);
@@ -29,6 +38,8 @@ const MeetTheGroomers = ({ options }) => {
                         />
                         <h5 key={choice.name}>{choice.name}</h5>
                         <p key={choice.bio}>{choice.bio}</p>
+
+                        <button onClick={event => window.location.href ="/Appointment"}>Click here to Book with {choice.name}</button>
                     </div>
                 ))}
             </ul>
